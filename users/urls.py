473986de8cd4b8da638bb8b3dac7dcfa70fe3848
Farah Quader <fyquader@gmail.com>
2019-05-09 13:37:58 -0700

    Fix url paths and register users

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.profile, name='user-profile'),
-    path('register', views.register, name='user-register')
+    # path('', views.profile, name='user-profile'),
+    # path('register', views.register, name='user-register')
 ]
