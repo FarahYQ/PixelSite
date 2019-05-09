ce2904116d02b565aa38f20b270e0543600a6f07
Farah Quader <fyquader@gmail.com>
2019-05-08 23:59:11 -0700

    Add register path

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.profile, name='user-profile')
+    path('', views.profile, name='user-profile'),
+    path('register', views.register, name='user-register')
 ]
