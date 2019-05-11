7aef66c9e5d638d01cd4e824d8288c49567895db
Farah Quader <fyquader@gmail.com>
2019-05-11 09:28:02 -0700

    Add post method for user creation in users view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .serializers import UserSerializer
+from rest_framework.views import APIView
 
-# Create your views here.
+class UserCreate(APIView):
+    def post(self, request, format='json'):
+        serializer = UserSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.create(validated_date=request.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
+
