8237c9222566cc67bf3f6784ce30505b4ddf51d7
Farah Quader <fyquader@gmail.com>
2019-05-22 23:52:34 -0700

    Add redux actions and corsheader

--- src/actions/session_actions.js
+++ src/actions/session_actions.js
@@ -1,27 +1,32 @@
 import * as SessionUtil from '../utils/session_util';
+import jwt_decode from "jwt-decode";
 
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER ";
 export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
 export const RECEIVE_USER_SIGN_IN = "RECEIVE_USER_SIGN_IN";
 export const RECEIVE_SESSION_ERRORS = "ECEIVE_SESSION_ERRORS";
 
-
-export const receiveCurrentUser = currentUser => ({
-    type: RECEIVE_CURRENT_USER,
-    currentUser
-})
-
-export const receiveUserSignIn = () => ({
-    type: RECEIVE_USER_SIGN_IN
-});
-
 export const receiveErrors = errors => ({
     type: RECEIVE_SESSION_ERRORS,
     errors
 });
 
+export const logoutUser = () => ({
+    type: RECEIVE_USER_LOGOUT
+})
+
 export const signup = (user) => dispatch => {
-    SessionUtil.signup(user)
-        .then(() => dispatch(receiveUserSignIn()))
-        .catch(err => dispatch(receiveErrors(err.response.data)))
+    SessionUtil.signup(user).then(res => {
+        const { token } = res.data;
+        localStorage.setItem("jwtToken", token);
+        const decoded = jwt_decoded(token);
+    }).catch(err => dispatch(receiveErrors(err.response.data)))
+};
+
+export const login = (user) => dispatch => {
+    SessionUtil.login(user).then(res => {
+        const { token } = res.data;
+        localStorage.setItem("jwtToken", token);
+        const decoded = jwt_decoded(token);
+    }).catch(err => dispatch(receiveErrors(err.response.data)))
 };
