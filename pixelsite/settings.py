33bfda2b99de0e069e0dec1cf54422ad4e042024
Farah Quader <fyquader@gmail.com>
2019-06-05 11:03:23 -0700

    Update settings

--- pixelsite/settings.py
+++ pixelsite/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "assets"),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, ’staticfiles’)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # cors whitelist
 CORS_ORIGIN_WHITELIST = 'http://127.0.0.1:8000',
