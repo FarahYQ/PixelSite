fdcd8d33f43a610d792857d18030911fb51707cb
Farah Quader <fyquader@gmail.com>
2019-07-02 21:50:17 -0700

    Update postgres database settings and api call

--- pixelsite/settings.py
+++ pixelsite/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -87,22 +86,11 @@ WSGI_APPLICATION = 'pixelsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-#         'NAME': os.environ.get('PG_NAME'),
-#         'USER': os.environ.get('PG_USER'),
-#         'PASSWORD': os.environ.get('PG_PASS'),
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-
-#     }
-# }
+# Need to export database_url locally
 import dj_database_url
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=500)
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
